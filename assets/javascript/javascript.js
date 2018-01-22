b13be35bebda7fb7adb8e656ecb3f3eaa411e4e4
serj <astralninja@gmail.com>
2018-01-21 22:46:24 -0600

    addition

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+
+
+
+var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+
+document.onkeyup = function(e){
+    if(e.keyCode == 32){
+        //generate word blanks
+        word="word"
+    }
+}
