23bb4653a8a5d5eda7d512c2686eb01bf948d746
serj <astralninja@gmail.com>
2018-01-22 18:58:10 -0600

    psychic change

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,54 @@
 
-
+var wins = 0;
+var losses = 0;
+var guesses = "";
+var guessesLeft = 9;
+var computerGuess;
 
 var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 
-document.onkeyup = function(e){
-    if(e.keyCode == 32){
-        //generate word blanks
-        word="word"
+var gameStarted = false
+
+function startGame(){
+	computerGuess = alphabet[Math.floor(Math.random()*alphabet.length)];
+	guesses = "";
+	guessesLeft = 9;
+	gameStarted = true;
+}
+
+function write(){
+	document.querySelector("#wins").innerHTML = wins;
+	document.querySelector("#losses").innerHTML = losses;
+	document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+	document.querySelector("#guesses").innerHTML = guesses;
+
+
+}
+
+
+document.onkeyup = function(event){
+	if (gameStarted == false){
+		startGame();
+	}
+    if(event.key == computerGuess){
+        wins++;
+        gameStarted= false;
+        write();
+
+        return
+    }
+    else{
+    	guesses += event.key;
+    	guessesLeft--;
+    	write();
+    	if (guessesLeft == 0){
+    		losses++;
+    		gameStarted= false;
+    		write();
+    		return
+    	}
+
+
     }
 }
